57cdfbace7ea468f4023f301642e2327d47013a0
Marcos Costa <marcoscosta9685@gmail.com>
2025-02-04 16:56:34 -0300

    Improved animations

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 // Função para sortear um Número aleatório
-function roll (min, max) {
-return Math.floor(Math.random() * (max - min + 1)) + min;
+function roll(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // Função para criar os dados com os números sorteador
-function addRoll (roll) {
-  const newRoll = document.createElement('div')
-      newRoll.classList.add('roll')
-      newRoll.append(roll)
+function addRoll(roll) {
+  const rollDiv = document.createElement('div')
+  rollDiv.classList.add('roll')
 
-      boardRolls.appendChild(newRoll)
-      return
+  const diceDiv = document.createElement('div')
+  diceDiv.classList.add('dice')
+
+  const diceBoxDiv = document.createElement('div')
+  diceBoxDiv.classList.add('dice-box')
+
+  const diceTextSpan = document.createElement('span')
+  diceTextSpan.classList.add('dice-text')
+  diceTextSpan.textContent = roll
+
+  diceBoxDiv.appendChild(diceTextSpan)
+  rollDiv.appendChild(diceDiv)
+  rollDiv.appendChild(diceBoxDiv)
+
+  rollsContainer.appendChild(rollDiv)
+  return
 }
 
 // Selecionando botões (sortear e sortear novamente)
@@ -26,7 +39,7 @@ const min_value = document.querySelector('#min_value')
 const max_value = document.querySelector('#max_value')
 
 // Selecionando o local onde vão ficar os resultados dos sorteios
-const boardRolls = document.querySelector('.rolls')
+const rollsContainer = document.querySelector('.rolls')
 
 // Selecionando as telas que irão sumir e aparecer (Quero sortear e Resultado do sorteio)
 const selectBoard = document.querySelector('.select')
@@ -44,12 +57,12 @@ let interations = 1
 submitForm.onsubmit = (event) => {
   // Evita que a página recarregue ao clicar no botão sortear
   event.preventDefault()
-  
+
   // Transformando os valores colocados nas opções de String para Number
   const amount = Number(amount_value.value)
   const min = Number(min_value.value)
   const max = Number(max_value.value)
-  
+
   // Vendo se o checkbox para números não repetidos foi selecionado
   const repeat = document.getElementById('repeat')
 
@@ -65,12 +78,12 @@ submitForm.onsubmit = (event) => {
     // Criando a condição: enquanto a quantidade de números sorteados for menor que a quantidade desejada, o programa repete
     while (numbersRolled.size < amount) {
       // Acionando a função de sortear um número
-      const rolled = roll(min,  max)
+      const rolled = roll(min, max)
 
       // Adicionando o número sorteado a lista de números que não podem ser repetidos. Caso seja sorteado um número repetido, ele não irá entrar aqui
       numbersRolled.add(rolled)
     }
-    
+
     // Transformando a lista de números que não podem ser repetidos em um Array
     const numberList = Array.from(numbersRolled)
 
@@ -87,7 +100,7 @@ submitForm.onsubmit = (event) => {
     // Criando uma estrutura para sortear a quantidade certa de números selecionado pelo usuario
     for (let i = 0; i < amount; i++) {
       // Acionando a função de sortear um número
-      const rolled = roll(min,  max)
+      const rolled = roll(min, max)
 
       // Acionando a função para criar os dados com os valores. O valor criado será o que tiver sido selecionado pela função anterior
       const add = addRoll(rolled)
@@ -96,7 +109,7 @@ submitForm.onsubmit = (event) => {
 }
 
 // Evento que acontece ao clicar em Sortear Novamente
-rollAgain.addEventListener('click', function(event) {
+rollAgain.addEventListener('click', function (event) {
   // Trocando a tela de Resultado do Sorteio para Quero Sortear
   rollingBoard.classList.add('hide')
   selectBoard.classList.remove('hide')
@@ -106,7 +119,7 @@ rollAgain.addEventListener('click', function(event) {
   interationsQuantity.textContent = `${interations}º resultado`
 
   // Removendo os dados com os valores para poder sortear novamente
-  while (boardRolls.firstChild) {
-    boardRolls.removeChild(boardRolls.firstChild);
+  while (rollsContainer.firstChild) {
+    rollsContainer.removeChild(rollsContainer.firstChild);
   }
 })
